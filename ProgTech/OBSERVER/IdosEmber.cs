7e844bcc46f4ae7b281e82bfd834995b52adbbd8
B. Péter <165163343+BgtPtr@users.noreply.github.com>
2024-04-28 23:08:27 +0200

    Add files via upload

---
+++ ProgTech/OBSERVER/IdosEmber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBSERVER
+{
+    class IdosEmber
+    {
+        public string Nev { get; set; }
+        public string Lakcim { get; set; }
+
+        public IdosEmber(string nev, string lakcim)
+        {
+            Nev = nev;
+            Lakcim = lakcim;
+        }
+    }
+}
